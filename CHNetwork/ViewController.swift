72ebcde15fa0e09dc0b8bacf502903c04cb79ab6
Ragaie alfy <Ragaie@ehabs-bc-macbook-pro.local>
2018-09-27 11:34:17 +0200

    Initial Commit

---
+++ CHNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CHNetwork
+//
+//  Created by Ragaie alfy on 9/27/18.
+//  Copyright © 2018 Ragaie alfy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
